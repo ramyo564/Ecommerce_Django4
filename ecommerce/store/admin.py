30d9e500b8b981b29e218e4d5eb685a97a23fec4
ramyo564 <yohan032yohan@gmail.com>
2023-05-04 02:15:22 +0900

    5) models
    static, image field, admin, slug

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Product
+
+# 함수기반
+# admin.site.register(Category)
+# admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin): 
+
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin): 
+
+    prepopulated_fields = {'slug': ('title',)}
